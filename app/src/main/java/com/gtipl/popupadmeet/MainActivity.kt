875afce602a4e634acf5a52977dc6831951e2b69
AnshuGTIPL <76987966+AnshuGTIPL@users.noreply.github.com>
2022-10-10 18:24:57 +0530

    Update MainActivity.kt

--- app/src/main/java/com/gtipl/popupadmeet/MainActivity.kt
+++ app/src/main/java/com/gtipl/popupadmeet/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.gtipl.popupadmeet
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import com.gtipl.meetadlink.MeetLinkModel
 
 class MainActivity : AppCompatActivity() {
+
+    var meetLinkView: MeetLinkModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    override fun onPause() {
+        super.onPause()
+        meetLinkView!!.hidePopView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            AlertDialog.Builder(this)
+                .setTitle("Permission Required..")
+                .setMessage("Keep widget calls upfront while using other apps. Simply give permission to draw over other apps")
+                .setPositiveButton("Grant Permission", { dialog, which ->
+
+                    val intent = Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:$packageName")
+                    )
+                    startActivityForResult(intent, 8888)
+                })
+                .show()
+        } else {
+
+            if (meetLinkView == null) {
+                meetLinkView = MeetLinkModel(this, windowManager, "https://vbuywith.com/Customer/widget/20")
+            }
+            meetLinkView!!.showPopView()
+
+        }
     }
 }
